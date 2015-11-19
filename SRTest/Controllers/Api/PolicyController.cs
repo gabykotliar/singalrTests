5a8a8258f33699cbf88263c67929d6d9d630f10c
Gaby Kotliar <gabriel.kotliar@globallogic.com>
2015-11-19 18:58:40 -0300

    update de estado de carga en FE con SignalR

---
+++ SRTest/Controllers/Api/PolicyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Web.Http;
+
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+using SRTest.SignalR;
+
+namespace SRTest.Controllers.Api
+{
+    [RoutePrefix("/api/policy")]
+    public class PolicyController : ApiController
+    {
+        public IHttpActionResult Get(int id)
+        {
+
+            var data = $"widget 1 para policyId {id}";
+
+            var hub = GlobalHost.ConnectionManager.GetHubContext<PolicyEditionHub>();
+
+            for (int widgetId = 1; widgetId < 10; widgetId++)
+            {
+                Thread.Sleep(500);
+                hub.Clients.All.widgetLoaded(widgetId);
+            }
+
+            return Json(new
+            {
+                id = id,
+                branchId = 200,
+                productId = 99
+            });
+        }
+    }
+}
+//DefaultHubManager hd = new DefaultHubManager(GlobalHost.DependencyResolver);
+
+//var hub3 = hd.ResolveHub("policySequence") as PolicyEditionHub;
+
+//hub3?.SendWidgetInfo(data);
---
+++ SRTest/SignalR/PolicyEditionHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SRTest.SignalR
+{
+    [HubName("policySequence")]
+    public class PolicyEditionHub : Hub
+    {
+        public void SendWidgetInfo(string widget)
+        {
+            Clients.All.widgetLoaded(widget);
+        }
+    }
+}
