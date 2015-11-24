bbf65f3fc5697a885779a173a35362cbe7754072
Gaby Kotliar <gabriel.kotliar@globallogic.com>
2015-11-24 10:26:50 -0300

    updated to call the load method directly in the hub. This allows the response to go to the caller only

--- SRTest/SignalR/DashboardHub.cs
+++ SRTest/SignalR/DashboardHub.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 
 namespace SRTest.SignalR
@@ -9,6 +15,30 @@ namespace SRTest.SignalR
         public void SendWidgetInfo(string widget)
         {
             Clients.All.widgetLoaded(widget);
+        }
+
+        public void Load(int policyId)
+        {
+            var tasks = new List<Task>();
+            var random = new Random(DateTime.Now.Second);
+
+            for (int widgetId = 1; widgetId < 10; widgetId++)
+            {
+                tasks.Add(Task.Factory.StartNew(data => {
+                    var rn = random.Next(1000, 2000);
+
+                    Thread.Sleep(rn);
+
+                    Clients.Caller.widgetLoaded(new
+                    {
+                        id = data,
+                        delayed = rn
+                    });
+                },
+                widgetId));
+            }
+
+            Task.WaitAll(tasks.ToArray());
         }
     }
 }
